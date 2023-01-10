b49053dadce09da79afb0745b49cb7dc3d371ca4
minseo-and <msp05070478@gmail.com>
2023-01-10 10:58:30 +0900

    timer 기능 구현

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -10,20 +10,31 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   int totalSeconds = 1500;
+  bool isRunning = false;
   late Timer timer;
 
-  void onTick() {
-
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds--;
+    });
   }
 
-  void onStartPressed(){
-      timer = Timer.periodic(
-          Duration(seconds: 1),
-              (timer) {
+  void onStartPressed() {
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick,
+    );
+    setState(() {
+      isRunning = true;
+    });
+  }
 
-              });
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
   }
 
   @override
@@ -36,24 +47,24 @@ class _HomeScreenState extends State<HomeScreen> {
               flex: 1,
               child: Container(
                 alignment: Alignment.bottomCenter,
-                child: Text('25:00',
+                child: Text(
+                  '$totalSeconds',
                   style: TextStyle(
                       color: Theme.of(context).cardColor,
                       fontSize: 89,
-                      fontWeight: FontWeight.w600
-                  ),),
-              )
-          ),
+                      fontWeight: FontWeight.w600),
+                ),
+              )),
           Flexible(
               flex: 2,
               child: Center(
                 child: IconButton(
                   iconSize: 120,
                   color: Theme.of(context).cardColor,
-                  onPressed: () {
-
-                  },
-                  icon: Icon(Icons.play_circle_outline),
+                  onPressed: isRunning ? onPausePressed
+                    : onStartPressed,
+                  icon: Icon(isRunning ? Icons.pause_circle_outline
+                      : Icons.play_circle_outline),
                 ),
               )),
           Flexible(
@@ -62,24 +73,33 @@ class _HomeScreenState extends State<HomeScreen> {
                 children: [
                   Expanded(
                     child: Container(
-                      decoration: BoxDecoration(color: Theme.of(context).cardColor,
-                      borderRadius: BorderRadius.circular(30),
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).cardColor,
+                        borderRadius: BorderRadius.circular(30),
                       ),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text('시간 관리',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                            color: Theme.of(context).textTheme.headline1!.color
-                          ),),
-                          Text('0',
+                          Text(
+                            '시간 관리',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .headline1!
+                                    .color),
+                          ),
+                          Text(
+                            '0',
                             style: TextStyle(
                                 fontSize: 58,
                                 fontWeight: FontWeight.w600,
-                                color: Theme.of(context).textTheme.headline1!.color
-                            ),),
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .headline1!
+                                    .color),
+                          ),
                         ],
                       ),
                     ),
